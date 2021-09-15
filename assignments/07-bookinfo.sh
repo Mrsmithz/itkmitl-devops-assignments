6c8bb9a5788c8769b5ff3314b3ba005fb1673683
Dechapon Nunsen <62070070@it.kmitl.ac.th>
2021-09-15 03:18:30 +0000

    update assignment 07

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-docker build -t ratings ~/ratings
-docker build -t details ~/details
-docker build -t reviews ~/reviews
-docker build -t productpage ~/productpage
-docker run -d --name mongodb -p 27017:27017 -v ~/ratings/databases:/docker-entrypoint-initdb.d bitnami/mongodb:5.0.2-debian-10-r2
+(cd ~/app/ratings && git pull) || git clone -b dev git@github.com:Mrsmithz/itkmitl-bookinfo-ratings.git ~/app/ratings
+(cd ~/app/details && git pull) || git clone git@github.com:Mrsmithz/itkmitl-bookinfo-details.git ~/app/details
+(cd ~/app/reviews && git pull) || git clone git@github.com:Mrsmithz/itkmitl-bookinfo-reviews.git ~/app/reviews
+(cd ~/app/productpage && git pull) || git clone git@github.com:Mrsmithz/itkmitl-bookinfo-productpage.git ~/app/productpage
+docker build -t ratings ~/app/ratings
+docker build -t details ~/app/details
+docker build -t reviews ~/app/reviews
+docker build -t productpage ~/app/productpage
+docker run -d --name mongodb -p 27017:27017 -v ~/app/ratings/databases:/docker-entrypoint-initdb.d bitnami/mongodb:5.0.2-debian-10-r2
 docker run -d --name ratings -p 8080:8080 --link mongodb:mongodb -e SERVICE_VERSION=v2 -e 'MONGO_DB_URL=mongodb://mongodb:27017/ratings' ratings
 docker run -d --name details -p 8081:8081 details
 docker run -d --name reviews -p 8082:8082 --link ratings:ratings -e 'RATINGS_SERVICE=http://ratings:8080' -e 'ENABLE_RATINGS=true' reviews

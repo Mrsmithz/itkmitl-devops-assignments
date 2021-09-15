9049c6818a7070ea550e26a6779e3041d80d20ae
Dechapon Nunsen <62070070@it.kmitl.ac.th>
2021-09-15 02:23:09 +0000

    update assignment 07

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+docker build -t ratings ~/ratings
+docker build -t details ~/details
+docker build -t reviews ~/reviews
+docker build -t productpage ~/productpage
+docker run -d --name mongodb -p 27017:27017 -v ~/ratings/databases:/docker-entrypoint-initdb.d bitnami/mongodb:5.0.2-debian-10-r2
+docker run -d --name ratings -p 8080:8080 --link mongodb:mongodb -e SERVICE_VERSION=v2 -e 'MONGO_DB_URL=mongodb://mongodb:27017/ratings' ratings
+docker run -d --name details -p 8081:8081 details
 docker run -d --name reviews -p 8082:8082 --link ratings:ratings -e 'RATINGS_SERVICE=http://ratings:8080' -e 'ENABLE_RATINGS=true' reviews
 docker run -d --name productpage -p 8083:8083 --link details:details --link ratings:ratings --link reviews:reviews -e 'REVIEWS_HOSTNAME=http://reviews:8082' -e 'RATINGS_HOSTNAME=http://ratings:8080' -e 'DETAILS_HOSTNAME=http://details:8081' productpage
